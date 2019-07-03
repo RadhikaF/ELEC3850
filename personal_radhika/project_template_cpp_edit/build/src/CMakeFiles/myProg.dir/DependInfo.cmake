008ef3068ce38de81903eb7618b7d6f7408ff4b6
RadhikaF <42195600+RadhikaF@users.noreply.github.com>
2019-07-03 17:14:30 +1000

    Add files via upload

---
+++ personal_radhika/project_template_cpp_edit/build/src/CMakeFiles/myProg.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/nodetest/project_template_cpp_edit/src/ELEC3850_client.cpp" "/home/pi/nodetest/project_template_cpp_edit/build/src/CMakeFiles/myProg.dir/ELEC3850_client.cpp.o"
+  "/home/pi/nodetest/project_template_cpp_edit/src/base64.cpp" "/home/pi/nodetest/project_template_cpp_edit/build/src/CMakeFiles/myProg.dir/base64.cpp.o"
+  "/home/pi/nodetest/project_template_cpp_edit/src/main.cpp" "/home/pi/nodetest/project_template_cpp_edit/build/src/CMakeFiles/myProg.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../src/src"
+  "../src/include"
+  "/usr/local/include/opencv4"
+  "../src/../common"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/pi/nodetest/project_template_cpp_edit/build/lib/socket.io-client-cpp/CMakeFiles/sioclient.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ personal_radhika/project_template_cpp_edit/build/src/CMakeFiles/myProg.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myProg.dir/main.cpp.o"
+  "CMakeFiles/myProg.dir/ELEC3850_client.cpp.o"
+  "CMakeFiles/myProg.dir/base64.cpp.o"
+  "../myProg.pdb"
+  "../myProg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myProg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
